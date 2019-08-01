b3afea6f4ca3b42429385703a6c9c7d098518d07
Christina Nguyen <christina.nguyen@publicissapient.com>
2019-08-01 15:29:25 -0400

    declared an insertToHistory utility to add extra benefits in case a weird value is passed for the state argument

--- undo-reset/src/useApp.js
+++ undo-reset/src/useApp.js
@@ -5,14 +5,31 @@ import { useReducer } from 'react'
 const initialState = {
     friends: [],
     history: [],
+    theme: 'light',
 }
+
+const insertToHistory = (state) => {
+    if (state && Array.isArray(state.history)) {
+        // do not mutate
+        const newHistory = [...state.history]
+        newHistory.push(state)
+        return newHistory
+    }
+    console.warn(
+        'WARNING! The state was attempting capture but something went wrong. Please check if the state is controlled correctly.',
+    )
+    return state.history || []
+}
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case 'set-theme':
+            return {...state, theme: action.theme, history: insertToHistory(state) }
         case 'add-friend':
             return {
                 ...state,
                 friends: [...state.friends, action.friend],
-                history: [...state.history, state],
+                history: insertToHistory(state),
             }
         case 'undo':
             {
@@ -20,6 +37,8 @@ const reducer = (state, action) => {
                 if (isEmpty) return state
                 return {...state.history[state.history.length - 1] }
             }
+        case 'reset':
+            return {...initialState, history: insertToHistory(state) }
         default:
             return state
     }
